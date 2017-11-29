8aa04ce0e73a99471a5d6599f2389c50bffbd79c
Ben Tristem <github@bentristem.com>
2017-11-29 20:54:52 +0000

    Finding cell under planeyCopter

---
+++ Assets/PathFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PathFollow : MonoBehaviour {
+
+    Tilemap tileMap;
+
+
+	// Use this for initialization
+	void Start () {
+        tileMap = FindObjectOfType<Tilemap>();
+	}   
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 cell = tileMap.WorldToCell(transform.position);
+        print(cell);
+	}
+}
