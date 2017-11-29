7cd97fca01c362a11f9b115dddc85dde703bed2e
Ben Tristem <github@bentristem.com>
2017-11-29 20:09:07 +0000

    Splash screen and "main menu"

---
+++ Assets/Asset Packs/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    [Tooltip("In seconds")][SerializeField] float loadDelay = 2.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadFirstScene", loadDelay);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
