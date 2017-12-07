6e993ac1c3559f2ddfc4f7cc8390fd64db719956
Ben Tristem <github@bentristem.com>
2017-12-07 12:23:02 +0000

    Detect device orientation

---
+++ Assets/Orient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Orient : MonoBehaviour {
+
+    [SerializeField] Text textBox;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+        {
+            textBox.text = "Landscape Left";
+        }
+        else
+        {
+            textBox.text = "Something else";
+        }
+	}
+}
