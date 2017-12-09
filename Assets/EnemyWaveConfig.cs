d3b4b8227b0516ff238847616aa519cc48545f83
Ben Tristem <github@bentristem.com>
2017-12-09 11:59:36 +0000

    Basic setup

---
+++ Assets/EnemyWaveConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Unit
+{
+    Red,
+    Green,
+    Blue
+}
+
+[CreateAssetMenu(menuName = "Wave")]
+public class EnemyWaveConfig : ScriptableObject
+{
+    public float[] triggerTimes;
+    public Unit[] units;
+}
---
+++ Assets/Scene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene : MonoBehaviour {
+
+    public EnemyWaveConfig[] waves;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
