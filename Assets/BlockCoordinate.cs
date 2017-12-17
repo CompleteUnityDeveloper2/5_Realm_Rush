c875f0ba95771557d8d656708ecd00c109c2597c
Ben Tristem <github@bentristem.com>
2017-12-17 18:22:21 +0000

    Printing debug coordinates

---
+++ Assets/BlockCoordinate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCoordinate : MonoBehaviour {
+
+    int xGridPos, zGridPos;
+
+    // Use this for initialization
+    void Start () {
+        xGridPos = Mathf.RoundToInt(transform.position.x);
+        zGridPos = Mathf.RoundToInt(transform.position.z);
+
+        TextMesh debugText = GetComponent<TextMesh>();
+        debugText.text = xGridPos + "," + zGridPos;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
