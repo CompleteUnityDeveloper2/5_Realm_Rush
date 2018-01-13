c49901897a9522d2c0c16b78a47a3c36dd52e96c
Ben Tristem <github@bentristem.com>
2018-01-13 14:41:27 +0000

    Snapping moved to code

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Block : MonoBehaviour {
 
     [SerializeField] Texture placeable;
     [SerializeField] Texture illegal;
     [SerializeField] bool isPlaceable = true;
+
+    Vector3 snapPos = new Vector3();
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +17,23 @@ public class Block : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        SnapToGrid();
+        LabelBlock();
+    }
+
+    private void LabelBlock()
+    {
+        Node node = new Node((int)snapPos.x, (int)snapPos.z);
+        TextMesh debugText = GetComponentInChildren<TextMesh>();
+        debugText.text = node.ToString();
+    }
+
+    private void SnapToGrid()
+    {
+        snapPos.x = Mathf.Round(transform.position.x / 10f) * 10f;
+        snapPos.z = Mathf.Round(transform.position.z / 10f) * 10f;
+        transform.position = snapPos;
+    }
 }
