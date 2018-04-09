bcf3c5a11275dfde1397f85aeddf545399252eb1
Rick <rickinaction@gmail.com>
2018-04-09 12:52:02 +1000

    Added scoreboard elements - enemies spawned and base health

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
 
@@ -8,10 +9,13 @@ public class EnemySpawner : MonoBehaviour {
     [SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] EnemyMovement enemyPrefab;
     [SerializeField] Transform enemyParentTransform;
+    [SerializeField] Text spawnedEnemies;
+    int score;
 
 	// Use this for initialization
 	void Start ()
     {
+        spawnedEnemies.text = score.ToString();
         StartCoroutine(RepeatedlySpawnEnemies());
 	}
 	
@@ -21,7 +25,14 @@ public class EnemySpawner : MonoBehaviour {
         {
             var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             newEnemy.transform.parent = enemyParentTransform;
+            AddScore();
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    void AddScore()
+    {
+        score++;
+        spawnedEnemies.text = score.ToString();
     }
 }
